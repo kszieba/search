8a8c20d062ad4d1950152263a90b949c68884b08
kszieba <85303178+kszieba@users.noreply.github.com>
2021-06-14 18:44:07 -0400

    Altered to correspond to new file naming scheme

--- test_ibs_algorithm.py
+++ test_ibs_algorithm.py
@@ -13,20 +13,20 @@ import blocksworld
 
 
 for i in range(1, 101, 20):
-    data, initstate = slidingtiles.read_file("C:/Users/melis/korf100/korf100/" + str(i))
+    data, initstate = slidingtiles.read_file("C:/Users/melis/korf100/" + str(i))
     filename = "korf100/" + str(i)
     for beam in range(20, 61, 20):
         for depth in range(70, 101, 10):
             search_algorithm(filename, initstate, data, beam, depth)
 
-for i in range(1, 5):
-    data, initstate = blocksworld.read_file("C:/Users/melis/blocksworld_puzzles/" +"Puzzle" + str(i) +".txt")
-    filename = "Blocks_world_puzzles/" + str(i)
+for name in ("4puzzle1", "9puzzle2", "12puzzle1", "15puzzle2", "18puzzle1"):
+    data, initstate = blocksworld.read_file("C:/Users/melis/blocksworld_puzzles/blocks" + name +".txt")
+    filename = "Blocks_world_puzzles/" + name
     for beam in range(20, 61, 20):
         for depth in range(70, 101, 10):
             search_algorithm(filename, initstate, data, beam, depth)
 for i in range(5, 30, 5):
-    data, initstate = slidingtiles.read_file("C:/Users/melis/8_puzzles/8_puzzles/" + str(i) +"42")
+    data, initstate = slidingtiles.read_file("C:/Users/melis/8_puzzles/" + str(i) +"42")
     filename = "8_puzzles/" + str(i) + "42"
     for beam in range(20, 60, 20):
         for depth in range(70, 100, 10):
