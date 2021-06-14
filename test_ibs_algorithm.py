337812953beb86ce9a213de925d85b8cf0c606a8
kszieba <85303178+kszieba@users.noreply.github.com>
2021-06-14 18:11:26 -0400

    Altered to take filename as a parameter in all places where search algorithm is called
    Previously raised an error due to the incorrect number of parameters taken in two of the code blocks. (One of which was previously commented out.)

--- test_ibs_algorithm.py
+++ test_ibs_algorithm.py
@@ -11,15 +11,13 @@ import slidingtiles
 
 import blocksworld
 
-"""
+
 for i in range(1, 101, 20):
     data, initstate = slidingtiles.read_file("C:/Users/melis/korf100/korf100/" + str(i))
     filename = "korf100/" + str(i)
     for beam in range(20, 61, 20):
         for depth in range(70, 101, 10):
-            search_algorithm(initstate, data, beam, depth)
-"""
-
+            search_algorithm(filename, initstate, data, beam, depth)
 
 for i in range(1, 5):
     data, initstate = blocksworld.read_file("C:/Users/melis/blocksworld_puzzles/" +"Puzzle" + str(i) +".txt")
@@ -32,4 +30,4 @@ for i in range(5, 30, 5):
     filename = "8_puzzles/" + str(i) + "42"
     for beam in range(20, 60, 20):
         for depth in range(70, 100, 10):
-            search_algorithm(initstate, data, beam, depth)
+            search_algorithm(filename, initstate, data, beam, depth)
