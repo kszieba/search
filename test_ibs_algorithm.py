dea735bd2f6b55af375d2a78d47aae2a6c673039
kszieba <85303178+kszieba@users.noreply.github.com>
2021-06-15 19:54:38 -0400

    Summarized program function in detail, including argument guide

--- test_ibs_algorithm.py
+++ test_ibs_algorithm.py
@@ -2,7 +2,17 @@
 """
 Created on Fri Jun 11 14:35:55 2021
 
-@author: melis
+This program is intended to test ibs search algorithms of various sorts. It
+can run tests utilizing the sliding tiles and blocksworld domains.
+
+The optional arguments it can take are as follows:
+
+-a (algorithm file)
+-s (size of test to be conducted) default small (large)
+-t (type of test to be conducted) default general (fixed_beam, fixed_depth)
+-p (parameter for type of test chosen) default None
+-d (domain type in lowercase) default None (sliding_tiles, blocksworld)
+
 """
 
 import argparse
@@ -12,28 +22,6 @@ import os.path
 import slidingtiles
 
 import blocksworld
-
-"""
-for i in range(1, 101, 20):
-    data, initstate = slidingtiles.read_file("C:/Users/melis/korf100/" + str(i))
-    filename = "korf100/" + str(i)
-    for beam in range(20, 61, 20):
-        for depth in range(70, 101, 10):
-            search_algorithm(filename, initstate, data, beam, depth)
-
-for name in ("4puzzle1", "9puzzle2", "12puzzle1", "15puzzle2", "18puzzle1"):
-    data, initstate = blocksworld.read_file("C:/Users/melis/blocksworld_puzzles/blocks" + name +".txt")
-    filename = "Blocks_world_puzzles/" + name
-    for beam in range(20, 61, 20):
-        for depth in range(70, 101, 10):
-            search_algorithm(filename, initstate, data, beam, depth)
-for i in range(5, 30, 5):
-    data, initstate = slidingtiles.read_file("C:/Users/melis/8_puzzles/" + str(i) +"42")
-    filename = "8_puzzles/" + str(i) + "42"
-    for beam in range(20, 60, 20):
-        for depth in range(70, 100, 10):
-            search_algorithm(filename, initstate, data, beam, depth)
-"""
             
 def test_algorithm(algorithm, size, giventype, parameter, domain):
     algor = __import__(algorithm)
