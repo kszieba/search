ae6302cc936abdadd61b79d332b1679f89bd9266
kszieba <85303178+kszieba@users.noreply.github.com>
2021-06-22 16:06:56 -0400

    Adjusted to import node and reverse node classes, as well as related functions

--- ibs_full_closed_list_variant.py
+++ ibs_full_closed_list_variant.py
@@ -9,178 +9,16 @@ import os.path
 
 import argparse
 
-from collections import OrderedDict
-
 from heap_with_keys import Heap_with_keys
 
 from red_black_tree import RedBlackTree
+
+from ibs_nodes import Node, push, popfirst, poplast, remove
 
 from guppy import hpy
 h = hpy()
 print(h.heap())
 
-
-class Node:
-
-    def __init__(self, state, g, parent, data):
-        self.state = state
-        self.g = g
-        self.f = g + state.heuristic(data)
-        self.parent = parent
-        self.key = str(self.state.key())
-        # could store level, but I don't see a point
-
-    def __lt__(self, b):
-        return self.f < b.f or (self.f == b.f and self.g > b.g)
-    
-    def __le__(self, b):
-        return self < b or (self.f == b.f and self.g == b.g)
-
-    def __gt__(self, b):
-        return self.f > b.f or (self.f == b.f and self.g < b.g)
-    
-    def __ge___(self, b):
-        return self > b or (self.f == b.f and self.g == b.g)
-
-    def __eq__ (self, b):
-        return self.key == b.key
-
-    def __hash__(self):
-        return hash(self.key)
-
-    def return_key(self):
-        return self.key
-
-    def print_backwards_path(self):
-        self.state.print_information()
-        if not self.parent:
-            return
-        self.parent.print_backwards_path()
-
-    def print_path(self, thelist):
-        self.collect_path(thelist)
-        print("Directions:")
-        for i in range(len(thelist)):
-            index = -1 * (i + 1)
-            if thelist[index] == "l":
-                print ("left")
-            if thelist[index] == "r":
-                print ("right")
-            if thelist[index] == "u":
-                print ("up")
-            if thelist[index] == "d":
-                print ("down")
-
-    def print_path_j (self, thelist):
-        self.collect_path (thelist)
-        print("Directions:")
-        for i in range(len(thelist)):
-            index = -1 * (i + 1)
-            if thelist[index] == "l":
-                print ("左")
-            if thelist[index] == "r":
-                print ("右")
-            if thelist[index] == "u":
-                print ("上")
-            if thelist[index] == "d":
-                print ("下")
-
-    def print_path_a (self, thelist):
-        self.collect_path (thelist)
-        print("Directions:")
-        for i in range(len(thelist)):
-            index = -1 * (i + 1)
-            if thelist[index] == "l":
-                print ("←")
-            if thelist[index] == "r":
-                print ("→")
-            if thelist[index] == "u":
-                print ("↑")
-            if thelist[index] == "d":
-                print ("↓")
-    """
-    def collect_path (self, thelist):
-        if not self.state._ntype:
-            return
-        thelist.append(self.state._ntype)
-        self.parent.collect_path(thelist)
-    """
-
-class RNode:
-
-    def __init__(self, state, g, parent, data):
-        self.state = state
-        self.g = g
-        self.f = g + state.heuristic(data)
-        self.parent = parent
-        self.key = str(self.state.key())
-        # could store level, but I don't see a point
-
-    def __lt__(self, b):
-        return self.f > b.f or (self.f == b.f and self.g < b.g)
-
-    def __gt__(self, b):
-        return self.f < b.f or (self.f == b.f and self.g > b.g)
-    
-    def __le__(self, b):
-        return self < b or (self.f == b.f and self.g == b.g)
-
-    def __ge__(self, b):
-        return self > b or (self.f == b.f and self.g == b.g)
-
-    def __eq__ (self, b):
-        return self.key == b.key
-
-    def __hash__(self):
-        return hash(self.key)
-
-    def return_key(self):
-        return self.key
-
-    def print_backwards_path(self, data):
-        self.state.print_information(data)
-        if not self.parent:
-            return
-        self.parent.print_backwards_path(data)
-
-
-def convertfromSNode(node, data):
-    rNode = RNode(node.state, node.g, node.parent, data)
-    return rNode
-
-def convertfromRNode(node, data):
-    sNode = Node(node.state, node.g, node.parent, data)
-    return sNode
-
-def push(listset, depth, mdepth, node, data):
-    if type(listset[depth]) != RedBlackTree:
-        #print("Hello, push")
-        #print(node.key)
-        listset[depth].push(node)
-        #print(depth + mdepth)
-        listset[depth + mdepth].push(convertfromSNode(node, data))
-    else:
-        listset[depth].insert(node)
-
-def popfirst(listset, depth, mdepth):
-    if type(listset[depth]) != RedBlackTree:
-        node = listset[depth].pop()
-        listset[depth + mdepth].remove(node)
-    else:
-        node = listset[depth].pop_min()
-    return node
-        
-def poplast(listset, depth, mdepth, data):
-    if type(listset[depth]) != RedBlackTree:
-        node = convertfromRNode(listset[depth + mdepth].pop(), data)
-        listset[depth].remove(node)
-    else:
-        node = listset[depth].pop_max()
-    return node
-
-def remove(listset, depth, mdepth, node):
-    listset[depth].remove(node)
-    listset[depth + mdepth].remove(node)
 
 def gen_move_children(current, actBW, waitBW, openlist,
     waitlist, closedlist, dep, mdepth, solution_c, data):
@@ -238,7 +76,7 @@ def search_algorithm (filename, startstate, data, bwidth, mdepth, call_type="sta
         openlist[i] = Heap_with_keys([])
         openlist[i + mdepth] = Heap_with_keys([])
         waitlist[i] = RedBlackTree()
-        closedlist[i] = OrderedDict()   #correspond to line 3
+        closedlist[i] = {}   #corresponds to line 3
     solution_c = mdepth #substitute for infinity to avoid an unreasonably high number of digits
     goal = None
     goalcount = 0
