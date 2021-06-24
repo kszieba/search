0d92230a82b18d0e6f152ecc048862147de15363
kszieba <85303178+kszieba@users.noreply.github.com>
2021-06-24 10:25:57 -0400

    Updated for increased amount of recorded information and for being unable to expect data for all four algorithms..
    Has not yet been tested.

--- script_for_plotting.py
+++ script_for_plotting.py
@@ -15,62 +15,29 @@ xvalues=[]
 for i in range(1, 101):
     xvalues.append(i)
 
-def plotMathFunction(yvalues, title):
-    plt.plot(yvalues)
-    plt.yscale('linear')
-    plt.title(title)
-    plt.show()
-    
-def plot(yvalues, title):
-    plt.plot(yvalues)
-    plt.yscale('linear')
-    plt.xlabel("Number of Elements in Sequence(n)")
-    plt.title(title)
-    plt.show()    
-    
-def plot2(yvalues, title, yvalues2):
-    plt.plot(yvalues)
-    plt.plot(yvalues2)
-    plt.title(title)
-    plt.show()   
-"""
-#2^10=O(1)   
-f1Yvalues=[]
-for i in range(1, 101):
-    f1Yvalues.append(pow(2,10)+0*i)
-plotMathFunction(f1Yvalues, "Plot of 2 Raised to the 10th Power") 
 
 
-#2log2n=O(n)
-f2Yvalues=[]
-for i in range(1, 101):
-    f2Yvalues.append((math.log2(i))*2)
-plotMathFunction(f2Yvalues, "Plot of 2 Times the Log of n") 
-"""
-
-#create sequence of length s for inputting
-def makeSequence(length):
-    sequence=[]
-    for i in range(0, length*3, 2):
-        sequence.append(1)
-    return sequence 
-
-
-def plot_variable(inputfile, domain, plot_type):
-    ibs_list = []
-    ibs_fcl_list = []
-    ibs_k_list = []
-    ibs_k_fcl_list = []
+def plot_variable(inputfile, domain, c_type, plot_type, algor1=None, algor2=None):
+    algorithms = ("ibs", "ibs_full_closed_list_variant", "ibs_katrina", "ibs_katrina_full_closed_list")
+    if c_type == "all":
+        algorlists = ([] for i in range(4))
+    elif c_type == "two":
+        algorlists = ([] for i in range(2))
+        two = []
+        algors = ("ibs", "ibs_fcl", "ibs_k", "ibs_kfcl")
+        for j in (4):
+            if algor1 == algors[j]:
+                two.append(algorithms[j])
+            if algor2 == algors[j]:
+                two.append(algorithms[j])
+            
     linecount = 0
     if domain == "sliding_tiles":
         domainname = "Sliding Tiles"
     else:
         domainname = "Blocksworld"
-    plot_types = ["" for j in range(18)]
-    plot_types[12] = "expandcount"
-    plot_types[13] = "gencount"
-    plot_types[14] = "time"
-    plot_types[15] = "memory"
+    plot_types = ("g_value", "expanded", "generated", "duplicates", 
+                  "reexpansions","max_open", "max_wait", "max_closed")
     for i in range(len(plot_types)):
         if plot_types[i] == plot_type:
             break
@@ -79,37 +46,31 @@ def plot_variable(inputfile, domain, plot_type):
         pieces = line.split(",")
         if pieces[16] != "Yes":
             if pieces[4] == domain:
-                if i in (12, 13, 15):
+                if c_type == "all":
                     if pieces[3] == "ibs":
-                        ibs_list.append(int(pieces[i]))
+                        algorlists[0].append(int(pieces[i+8]))
                     elif pieces[3] == "ibs_full_closed_list_variant":
-                        ibs_fcl_list.append(int(pieces[i]))
+                        algorlists[1].append(int(pieces[i+8]))
                     elif pieces[3] == "ibs_katrina":
-                        ibs_k_list.append(int(pieces[i]))
+                        algorlists[2].append(int(pieces[i+8]))
                     elif pieces[3] == "ibs_katrina_full_closed_list":
-                        ibs_k_fcl_list.append(int(pieces[i]))
-                elif i in (14,):
-                    if pieces[3] == "ibs":
-                        ibs_list.append(float(pieces[i]))
-                    elif pieces[3] == "ibs_full_closed_list_variant":
-                        ibs_fcl_list.append(float(pieces[i]))
-                    elif pieces[3] == "ibs_katrina":
-                        ibs_k_list.append(float(pieces[i]))
-                    elif pieces[3] == "ibs_katrina_full_closed_list":
-                        ibs_k_fcl_list.append(float(pieces[i]))
+                        algorlists[3].append(int(pieces[i+8]))
+                elif c_type == "two":
+                    if pieces[3] == two[0]:
+                        algorlists[0].append(int(pieces[i+8]))
+                    elif pieces[3] == two[1]:
+                        algorlists[1].append(int(pieces[i+8]))
                 linecount += 1
     count = [0 for j in range(linecount//4)]
-    for j in range(linecount//4):
+    beams = (10, 20, 40, 60, 80, 100)
+    for j in len(beams):
         count[j] = j
-    plt.plot(count, ibs_list)
-    plt.plot(count, ibs_fcl_list)
-    plt.plot(count, ibs_k_list)
-    plt.plot(count, ibs_k_fcl_list)
-    titlelist = plot_types = ["" for j in range(18)]
-    titlelist[12] = "Nodes Expanded"
-    titlelist[13] = "Nodes Generated"
-    titlelist[14] = "Cumulative Time (sec)"
-    titlelist[15] = "Cumulative Memory (bytes)"
+    for algorithm in algorlists:
+        plt.plot(count, algorithm)
+    titlelist = plot_types = ["G-Value of Solution", "Nodes Expanded", 
+            "Nodes Generated", "Duplicates Pruned", "Re-expansions", 
+            "Maximum Size of Open List", "Maximum Size of Wait List"
+            "Maximum Size of Closed List"]
     plt.title("Plot of "+ titlelist[i] + " for " + domainname)
     plt.show()
 
@@ -120,12 +81,26 @@ def plot_data(inputfile, plot_type, domain):
 if __name__=='__main__':
     PARSE = argparse.ArgumentParser()
     PARSE.add_argument("-i", help='input file path (shortened)', type=str)
-    #PARSE.add_argument("-o", help='output file path (shortened)', type=str)
+    PARSE.add_argument("-c", help='comparison type', type=str)
     PARSE.add_argument("-t", help='plot type', type=str)
     PARSE.add_argument("-d", help='domain', type=str)
+    PARSE.add_argument("-f", help='first algorithm', type=str, required=False)
+    PARSE.add_argument("-s", help='second algorithm', type=str, required=False)
     arguments = PARSE.parse_args()
     if not arguments.i:
         inputfile = "ibs_results2.csv"
         plot_type = "time"
         domain = "blocksworld"
-    plot_data (inputfile, plot_type, domain)
+        c_type = "all"
+        plot_data (inputfile, c_type, plot_type, domain)
+    else:
+        inputfile = arguments.i
+        c_type = arguments.c
+        plot_type = arguments.t
+        domain = arguments.d
+        if c_type == "two":
+            algor1 = arguments.f
+            algor2 = arguments.s
+            plot_data (inputfile, c_type, plot_type, domain, algor1, algor2)
+        else:
+            plot_data (inputfile, c_type, plot_type, domain)
