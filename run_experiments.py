7f4e44b453d0501568deeb2fc3e88b8edc167b3e
kszieba <85303178+kszieba@users.noreply.github.com>
2021-06-24 14:37:29 -0400

    Now simply solve problems and print the output from them to individual files

--- run_experiments.py
+++ run_experiments.py
@@ -26,70 +26,16 @@ def run_experiments(algorithm, domain, depth, folder, start, tail):
         aname = "ibs_katrina_full_closed_list" 
     algor_file = __import__(aname)
     stdout = sys.stdout
-    wnames = []
     for i in range(start, tail + 1):
         data, initstate = read_file("C:/Users/melis/" + folder + "/" + str(i))
-        wname = "_".join([algorithm, folder, str(i), "beam_set", str(depth) + ".txt"])
-        wnames.append(wname)
         beams = (10, 20, 40, 60, 80, 100)
         filepath = folder + "/" + str(i)
-        wfile = open("New_Results/" + wname, "w")
-        sys.stdout = wfile
         for j in range(6):
-            """
-            print(j, file=sys.stderr)
-            print(beams[j], file=sys.stderr)
-            """
+            wname = "_".join([algorithm, folder, str(i), str(beams[j]), str(depth) + ".txt"])
+            wfile = open("New_Results/Single_Results/" + wname, "w")
+            sys.stdout = wfile
             algor_file.search_algorithm(filepath, initstate, data, beams[j], depth)
-        wfile.close()
-    finalname = "_".join([folder, str(start), str(tail), algorithm, str(depth) + ".csv"])
-    finalfile = open(finalname, "w")
-    sys.stdout = finalfile
-    for i in range(start, tail + 1):
-        rname = wnames[i - start]
-        rfile = open("New_Results/" + rname, "r")
-        resultcount = 0
-            #print(rname, file=sys.stderr)
-        print(",".join([folder, str(i), str(data), rname, aname, str(beams[resultcount]), str(depth), domain]), end=",")
-        count = -1
-        collect = ""
-        correct = False
-        lines_to_keep = (4, 6, 9, 10, 11, 12, 13, 14, 15)
-        collected = 0
-        for line in rfile:
-            count += 1 #count has the right numbers
-            #print(count, file=sys.stderr)
-            if collected == 9:
-                print("")
-                resultcount += 1
-                if resultcount == 6:
-                    break
-                collected = 0
-                count = -1
-                print(",".join([folder, str(i), str(data), rname, aname, str(beams[resultcount]), str(depth), domain]), end=",") #runs five times, as it should
-                #print("Hello?", file=sys.stderr)
-                continue
-            elif count == lines_to_keep[collected]:
-                for j in range(len(line)):
-                    if line[j-1] == ":":
-                        correct = True
-                        continue
-                    if correct:
-                        if line[j].isnumeric():
-                            collect += line[j]
-                        elif line[j] == "\n":
-                            print(collect, end=",") #only five printed in the file
-                            """
-                            if lines_to_keep[k]==6:
-                                print(collect, file=sys.stderr) #six here, correct
-                            """
-                            correct = False
-                            collected += 1
-                            collect = ""
-                            break
-        rfile.close()
-        #print("Finished reading", file=sys.stderr)
-    finalfile.close()
+            wfile.close()
     sys.stdout = stdout
 
 if __name__=='__main__':
